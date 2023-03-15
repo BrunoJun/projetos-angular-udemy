9364d3a5ff96dc700bf9dea12e5430ff6aa0cdbe
BrunoJun <brunojun7@gmail.com>
2023-03-15 10:02:19 -0300

    Implementa funcionalidade de mudar para o tema escuro

--- Projetos/dark-theme/src/app/core/components/header/header.component.ts
+++ Projetos/dark-theme/src/app/core/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ETheme } from '../../enums/ETheme.enum';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,22 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  public icon: string = ETheme.ICON_MOON
+  public textTheme: string = ETheme.TEXT_MOON;
+
   public toggle(){
 
-    document.body.classList.toggle('dark-theme')
+    const theme = document.body.classList.toggle('dark-theme')
+
+    if (theme){
+
+      this.textTheme = ETheme.TEXT_SUN
+      this.icon = ETheme.ICON_SUN
+    } else {
+
+      this.textTheme = ETheme.TEXT_MOON
+      this.icon = ETheme.ICON_MOON
+    }
   }
 
 }
